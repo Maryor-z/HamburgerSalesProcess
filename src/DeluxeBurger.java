a0627ac225861ff926747fac3110266cb6b24d9e
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-16 21:14:06 +0100

    Update DeluxeBurger.java

--- src/DeluxeBurger.java
+++ src/DeluxeBurger.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class DeluxeBurger {
+public class DeluxeBurger extends Hamburger{
+
+    public DeluxeBurger() {
+
+        super("basic", "chicken", "white", 10.10);
+        super.addHamburgerAddition1("Chips", 5);
+        super.addHamburgerAddition2("Drink", 4);
+    }
+
+    public void addHamburgerAddition1(String addition1Name, double addition1Price){
+        System.out.println("Cannot add additional items to a deluxe burger.");
+    }
+
+    public void addHamburgerAddition2(String addition2Name, double addition2Price){
+        System.out.println("Cannot add additional items to a deluxe burger.");
+    }
+
+    public void addHamburgerAddition3(String addition3Name, double addition3Price){
+        System.out.println("Cannot add additional items to a deluxe burger.");
+    }
+
+    public void addHamburgerAddition4(String addition4Name, double addition4Price){
+        System.out.println("Cannot add additional items to a deluxe burger.");
+    }
+
+
 }
+
